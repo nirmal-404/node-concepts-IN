f60393de8c06684e7f2c49317047f3c4dd3aaaa4
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-03-12 05:09:21 +0530

    data structures-redis
    strings, lists, sets, sorted sets, hash sets

--- redis/data-structures.js
+++ redis/data-structures.js
@@ -6,7 +6,6 @@ const client = redis.createClient({
     host: 'localhost',
     port: 6379
 });
-// TODO
 // event listener
 client.on('error', (err) => {
     console.log('Redis client error ' + err);
